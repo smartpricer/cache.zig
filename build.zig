c4612d7c3da8215bb5df2f8f787c63a298582d84
Felix Kollmann <mail@fkollmann.de>
2024-02-04 12:53:10 +0100

    Change to be compatible with Zig 0.11

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
 	const optimize = b.standardOptimizeOption(.{});
 
 	_ = b.addModule("cache", .{
-		.root_source_file = .{ .path = "src/cache.zig" },
+		.source_file = .{ .path = "src/cache.zig" },
 	});
 
 	const lib_test = b.addTest(.{
